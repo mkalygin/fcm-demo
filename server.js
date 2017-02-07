ebddaa36d8405b957e08b0a713f2f5117d8c0a81
Michael Kalygin <michael.kalygin@gmail.com>
2017-02-07 16:36:56 +0300

    Setup static html page for demo

--- server.js
+++ server.js
@@ -3,12 +3,17 @@
 const config = require('./config');
 const Koa    = require('koa');
 const Router = require('koa-router');
+const send   = require('koa-send');
 
 const koa = new Koa();
 const app = new Router();
 
 app.get('/:name', async (ctx) => {
   ctx.body = `Hello, ${ctx.params.name}!\n`;
+});
+
+app.get('/', async (ctx) => {
+  await send(ctx, 'src/index.html');
 });
 
 koa.use(app.routes());
