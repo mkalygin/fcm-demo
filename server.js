5d422cf91c5e1c60afebea9528db79b3d8773256
Michael Kalygin <michael.kalygin@gmail.com>
2017-02-07 21:29:03 +0300

    Setup endpoint for sending push notifications

--- server.js
+++ server.js
@@ -3,9 +3,20 @@
 const path   = require('path');
 const config = require('./config');
 const Koa    = require('koa');
+const Router = require('koa-router');
 const serve  = require('koa-static');
 
 const koa = new Koa();
+const app = new Router();
 
 koa.use(serve(path.join(__dirname, 'public')))
+
+app.get('/push', async (ctx) => {
+  const secs = ctx.query.sleep || 0;
+  ctx.body = `push ${secs}`;
+});
+
+koa.use(app.routes());
+
+console.log(`Starting server on port ${config.port}...`);
 koa.listen(config.port);
