284d253d5f754108c9fa5d21a3692cfc17a81e2b
Michael Kalygin <michael.kalygin@gmail.com>
2017-02-07 17:17:34 +0300

    Setup firebase

--- public/app.js
+++ public/app.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey:            "AIzaSyDtSHj38TkFZUbVk7sYID8Vme5CKQzBjuQ",
+  authDomain:        "fcm-demo-821ab.firebaseapp.com",
+  databaseURL:       "https://fcm-demo-821ab.firebaseio.com",
+  storageBucket:     "fcm-demo-821ab.appspot.com",
+  messagingSenderId: "1031559967118"
+};
+
+firebase.initializeApp(config);
+
 function onOnSiteNotificationClick() {
   alert('on-site');
 }
