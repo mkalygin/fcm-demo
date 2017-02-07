66bae5d04eaa01c2fb61b6471ae03056ee92d5c6
Michael Kalygin <michael.kalygin@gmail.com>
2017-02-07 16:54:55 +0300

    Setup simple static server instead

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 'use strict';
 
+const path   = require('path');
 const config = require('./config');
 const Koa    = require('koa');
-const Router = require('koa-router');
-const send   = require('koa-send');
+const serve  = require('koa-static');
 
 const koa = new Koa();
-const app = new Router();
 
-app.get('/:name', async (ctx) => {
-  ctx.body = `Hello, ${ctx.params.name}!\n`;
-});
-
-app.get('/', async (ctx) => {
-  await send(ctx, 'src/index.html');
-});
-
-koa.use(app.routes());
+koa.use(serve(path.join(__dirname, 'public')))
 koa.listen(config.port);
